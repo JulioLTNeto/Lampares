fe7c312eca3a82648d16f3f42ca928b847918c5e
JulioLTNeto <41303671+JulioLTNeto@users.noreply.github.com>
2018-12-09 13:46:00 -0300

    Add files via upload

--- sensor1.ino
+++ sensor1.ino
@@ -1,25 +1,40 @@
 int sensor = 9;
+int sensor2 = 10;
+int sensor3 = 11;
+int sensor4 = 12;
 int led = 8;
 int ultimo = 0;
+int ledA = 13;
 unsigned long tempoInicial = 0;
 unsigned long tempoFinal = 0;
 
 void setup() {
   pinMode(sensor, INPUT);
+  pinMode(sensor2, INPUT);
+  pinMode(sensor3, INPUT);
+  pinMode(sensor4, INPUT);
   pinMode(led, OUTPUT);
+  pinMode(ledA, OUTPUT);
 }
 
 void loop() {
   int achou = digitalRead(sensor);
+  int achou2 = digitalRead(sensor2);
+  int achou3 = digitalRead(sensor3);
+  int achou4 = digitalRead(sensor4);
 
   tempoFinal = millis();
   if(tempoFinal - tempoInicial >= 6000){
     tempoInicial = tempoFinal;
-    digitalWrite(led, LOW);
+    digitalWrite(led, HIGH);
+    digitalWrite(ledA, LOW);
+    Serial.println("Fechar");
   }
   
-  if(achou){
-    digitalWrite(led, HIGH);
+  if(achou || achou2 || achou3 || achou4){
+    Serial.println("Detectou, reiniciando contagem");
+    digitalWrite(led, LOW);
+    digitalWrite(ledA, HIGH);
     tempoInicial = tempoFinal;
   }
   
